bf8c68649b812d1c80276d00090a10641c3ee913
Scott Baldwin <scott@wellmatchhealth.com>
2014-05-21 08:59:03 -0500

    Adding basic auth

--- app.rb
+++ app.rb
@@ -4,6 +4,12 @@ require 'securerandom'
 module Callings
   class App < Sinatra::Base
 
+    use Rack::Auth::Basic, "Protected Area" do |username, password|
+      ENV['AUTH_USER'] && ENV['AUTH_PASS'] &&
+      username == ENV['AUTH_USER'] &&
+      password == ENV['AUTH_PASS']
+    end
+
     get "/assets/js/application.js" do
       content_type :js
       @uuid = request.params['name'] || SecureRandom.uuid
